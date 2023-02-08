94f543629ebbcb38a960f1fadb24235417f137c9
Diego Alves <diegoalvesdc12@gmail.com>
2023-02-08 16:06:04 -0300

    add tela post

--- pages/post.php
+++ pages/post.php
@@ -1,91 +1,70 @@
-<?php session_start()?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/post.css">
-    <title>postagem</title>
-</head>
-<body>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 
-<?php include_once('../components/header.php') ?>
-
-
-<h1 class="last-post-title">Anúncios</h1>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="/styles/post.css">
+</head>
+<body>
+  <?php include_once("../components/header.php")?>;
+  <main>
+    <div class="main-top">
+      <div class="top-left">
+        <h1>Titulo do Anúncio</h1>
+        <p class="post-infos">Publicado em 10/02/23 às 09:30</p>
+        <div class="img-box">
+          <img src="https://assets3.repassa.com.br/fit-in/0x600/filters:sharpen(0.5,0.5,true)/spree/products/2338700/original/IMG_3565.JPG" alt="">
+        </div>
+      </div>
+      <div class="top-right">
+        <div class="price-box">
+          <span>R$ 394</span>
+        </div>
 
-    <main>
-        <div class="posts-group"> 
-        
-          <a class="post-item" hr ef="">
-            <div class="post-img-box">
-              <img src="https://auhekids.com.br/wp-content/uploads/2020/03/conjunto-batizado-jardineira-body-gravata1-829x800.jpeg" alt="" class="img-post">
-            </div>
-            <div class="post-text-box">
-              <div class="post-text-top">
-                <h2>
-                  Conjunto para menino
-                </h2>
-                <p class="post-price">R$50,50</p>
+        <div class="box-right">
+          
+          <div class="desc-box">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, minima. Neque magni, pariatur ullam ex fuga temporibus dicta illum iure ab ipsam odio exercitationem. At minus dolor optio? Cum, delectus.</p></p>
+          </div>
+
+          <div class="main-down">
+            <h2>Detalhes</h2>
+            <div class="details-box">
+              <div class="detail-box">
+                <p class="detail-title">Nome Vendedor</p>
+                <span class="detail-desc">Marcelly</span>
               </div>
-              <div class="post-text-bottom">
-                <p>Colombo - PR</p>
-                <span>Hoje, 20:20</span>
+
+              <div class="detail-box">
+                <p class="detail-title">Telefone</p>
+                <span class="detail-desc">4198438431</span>
               </div>
-            </div>
-          </a>
-        
-          <a class="post-item" href="">
-            <div class="post-img-box">
-              <img src="https://assets3.repassa.com.br/fit-in/0x600/filters:sharpen(0.5,0.5,true)/spree/products/2338700/original/IMG_3565.JPG" alt="" class="img-post">
-            </div>
-            <div class="post-text-box">
-              <div class="post-text-top">
-                <h2>
-                  Camisa masculina estampada
-                </h2>
-                <p class="post-price">R$94,90</p>
+
+              <div class="detail-box">
+                <p class="detail-title">Cidade</p>
+                <span class="detail-desc">Curitiba</span>
               </div>
-              <div class="post-text-bottom">
-                <p>Araucária - PR</p>
-                <span>Omtem, 12:20</span>
-              </div>
-            </div>
-          </a>
-        
-        
-          <a class="post-item" href="">
-            <div class="post-img-box">
-              <img src="https://ireland.apollo.olxcdn.com/v1/files/2rvy6b1knfie3-PT/image;s=3000x4000" alt="" class="img-post">
-            </div>
-            <div class="post-text-box">
-              <div class="post-text-top">
-                <h2>
-                  Sapatilha semi-nova
-                </h2>
-                <p class="post-price">R$75,00</p>
-              </div>
-              <div class="post-text-bottom">
-                <p>Curitiba - PR</p>
-                <span>Hoje, 18:20</span>
+              
+              <div class="detail-box">
+                <p class="detail-title">Bairro</p>
+                <span class="detail-desc">Campo Santana</span>
               </div>
             </div>
-          </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="line"></div>
+    
+  </main>
 
-
-    </main>
-
-
-  
-
-
-    <?php  include_once('../components/footer.php') ?>
-
+  <?php include_once("../components/footer.php")?>;
 
 </body>
 </html>
